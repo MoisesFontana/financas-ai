65e54ca7fa453834c96d311563e5ac94cae64132
Moises Fontana <mf@m4.local>
2026-06-10 20:38:21 -0300

    feat: criado rotas, header e botoes

---
+++ src/router.tsx
@@ -0,0 +1,31 @@
+import { PiggyBank } from 'lucide-react';
+import { createBrowserRouter } from 'react-router-dom';
+import { RootLayout } from './components/layout/RootLayout';
+import { Button } from './components/shared/Button';
+
+export const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: (
+          <>
+            <h1>Formulário de Simulação</h1>
+            <Button variant="primary" icon={PiggyBank}>
+              Clique aqui
+            </Button>
+          </>
+        ),
+      },
+      {
+        path: '/resultado',
+        element: <h1>Resultado da Simulação</h1>,
+      },
+      {
+        path: '/historico',
+        element: <h1>Histórico de Simulações</h1>,
+      },
+    ],
+  },
+]);
